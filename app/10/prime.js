61fd225b6711ea2e9960b921b5d108d9b93407bc
t14491st <sonosuke.takao@gmail.com>
2014-11-13 12:18:35 +0900

    課題回答

--- app/10/prime.js
+++ app/10/prime.js
@@ -8,19 +8,16 @@ var prime = function(a){
 var b
  b = 2 ;
 
-while(a = b)
-if(a % b == 0){
-  return false
-  b + 1
-}else{
-  return true
+while(a > b){
+    if(a % b == 0){
+
+        return false
+      }b = b + 1
+    }
+    return true
 }
-
-
-  if(a % 1 != 0 && a < 1){
-    return false
-  }
+if (a  %  1 !== 0 && a < 1){
   return false
   }
-  return false
+return false
 }
